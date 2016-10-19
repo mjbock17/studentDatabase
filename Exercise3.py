549d7362b6bbdf0fcb33f50e96d12136243a4a10
alexsocheathson <alexson28@gmail.com>
2016-10-19 18:22:58 -0500

    Update Exercise3.py

--- Exercise3.py
+++ Exercise3.py
@@ -1,11 +1,43 @@
-def CourseRetrieval(): # Finds the course number and displays the score information of that course
-    course = str(input("Please input the requested class: "))
+def CourseRetrieval():  #Takes in student ID and course number as inputs and displays the score for the indicated class
+    ID = str(input("Please input Student ID: "))
+    course = str(input("Please input Course Number: "))
 
-    allInformationStr = IDRetrieval()
-    studentInfoList = allInformationStr.split("\n")
-    heading = studentInfoList[0].split()[8:]    #creates the heading of the course data
+    studentIDList = readFile('StudentInfo2.txt','r',8)
     
-    print(heading)
+    courseHeaderList = studentIDList[0][3:] #List which is later used to create the header of the table.
+
+    headerCounter = 3    #initialization of varibles used for counters for print statements used in loops 
+    header =""
+    printCounter = 3
+    studentGrades = ""
+
+    while(headerCounter<8): #Displays the header 
+        header = header + "\t" + studentIDList[0][headerCounter]
+        headerCounter = headerCounter + 1
+    print (header)
     
-#I started over because after making the IDRetrieval method return a statement I forgot that that IDRetrival only returned the one class.
-#So I stopped on line 7. I wil start over making this method independent of ID retrieval.    
+    counter = 1
+    found = True   #used by the loops to determine whether or not to end 
+
+    while(counter<(len(studentIDList)-1)):   #checks for the indicated student ID 
+        if ID == studentIDList[counter][0]:
+            break
+        else:
+            counter = counter + 1
+            found = False
+
+    if found == False:
+        print("There is no student associated with this ID. ")
+    else:
+        if course == studentIDList[counter][3]:  #checks for the indicated class and prints the assocaited scores. 
+            while(printCounter<8):
+                studentGrades = studentGrades + "\t" + studentIDList[counter][printCounter]
+                printCounter = printCounter + 1
+            print(studentGrades)
+        elif course == studentIDList[counter+1][3]:
+            while(printCounter<8):
+                studentGrades = studentGrades + "\t" + studentIDList[counter+1][printCounter]
+                printCounter = printCounter + 1
+            print(studentGrades)
+        else:
+            print("This student exists but the class does not. ")
