2a50b0d3401d63a28d594fce1c965b3230e365c2
mjbock17 <mjbock@live.com>
2016-10-19 23:19:43 -0400

    Bug in IDRetrieval
    Was not calling course information correctly. have fixed. Please check with other values before merging with master

--- ReadingStudentData.py
+++ ReadingStudentData.py
@@ -12,6 +12,7 @@ def readFile(data,row,length): ###row=Read or Write, length=number of table head
     return datalist
 
 def mainMenu():
+
     Exit=True
     while Exit:
         Selection = input(" Welcome to the Student Database. Please select a number: \n" +
@@ -20,42 +21,44 @@ def mainMenu():
         if Selection=='1':
             IDRetrieval() 
         elif Selection=='2':
-            IDRetrieval()##Add function
+            ID1 = str(input("Please input Student ID: "))
+            YearRetrieval(ID1)
         elif Selection=='3':
             IDRetrieval()## addfunction
         elif Selection=='4':
             IDRetrieval()##add fucntion
         elif Selection=='5':
-            IDRetrieval()##add fucntion
+            Modifier()
         elif Selection=='6':
             Exit=False ##exits out
     
 def IDRetrieval(): ###Finds ID and displays information
+
     ID = str(input("Please input Student ID: "))
     endofList = 0
     
-    file1 = readFile('StudentInfo.txt','r',6)
+    file1 = readFile('StudentInfo.txt','r',7)
     file2 = readFile('StudentInfo2.txt','r',8)
     
     while len(file1)>endofList: ###This section finds the student info
-        if ID == file1[endofList][0]: 
-            print(" ".join(file1[0]) + " " + " ".join(file2[0][1:]) + "\n" + "\t".join(file1[endofList]) + "  " + "\t".join(file2[endofList][1:])) ###Initial Heading/First Semester Listed. Use of Join
+        if ID == file1[endofList][0]:
+            indexvalue=endofList
+##            print(" ".join(file1[0]) + " " + " ".join(file2[0][1:]) + "\n" + "\t".join(file1[endofList]) + "  " + "\t".join(file2[endofList][1:])) ###Initial Heading/First Semester Listed. Use of Join
             break
         else:
             endofList+=1
         if endofList == len(file1):
             ID = str(input("No ID exists, Please enter a new ID: "))### Checks for ID. Sends user back to the while statement if ID incorrectly entered
             endofList = 0
-            
-    ###Checking for extra semesters of courses###
-    ClassCheck = 0
+
+    ClassCheck = 0   ###Checking for extra semesters of courses###
+    PrintHeading=True ###Will make sure Heading prints once
     while len(file2)>ClassCheck:
-        if ID == file2[ClassCheck][0]:
-            while ID == file2[ClassCheck+1][0]:
-                print(("\t" * 8) + "\t".join(file2[ClassCheck+1][1:])) ###Prints only relevent semesters of courses pertaining to ID
-                ClassCheck+=1
+        if ID == file2[ClassCheck][0] and PrintHeading==False: ###Checks if Heading has been printed
+            print(("\t" * 8) + "\t".join(file2[ClassCheck][1:])) ###Prints only relevent semesters of courses pertaining to ID
+        if ID == file2[ClassCheck][0] and PrintHeading==True:
+            print(" ".join(file1[0]) + " " + " ".join(file2[0][1:]) + "\n" + "\t".join(file1[indexvalue]) + "  " + "\t".join(file2[ClassCheck][1:])) ###Initial Heading/First Semester Listed. Use of Join
+            PrintHeading=False
         ClassCheck+=1
-
-
 
 mainMenu()
