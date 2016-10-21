0db73c85ea8ec5a31bc5b40629b9dce70ca28d20
alexsocheathson <alexson28@gmail.com>
2016-10-20 23:45:23 -0500

    Update almostFinalProject.py

--- almostFinalProject.py
+++ almostFinalProject.py
@@ -17,7 +17,7 @@ def mainMenu():
     while Exit:
         Selection = input(" Welcome to the Student Database. Please select a number: \n" +
                          '1. All Information \n' + '2. Course Grades \n' + '3. Semester Classes and Grades \n' +
-                          '4. Maximum and Minimum Grades \n' + '5. Add, Remove or Change Student \n' + '6. Student Year \n' + '7. Exit \n')
+                          '4. Maximum and Minimum Grades \n' + '5. Add, Remove or Change Student \n' + '6. Student Year \n' + '7. Sorted Information \n' + '8. Exit \n')
         if Selection=='1':
             IDRetrieval() 
         elif Selection=='2':
@@ -32,6 +32,8 @@ def mainMenu():
             ID1 = str(input("Please input Student ID: "))
             YearRetrieval(ID1)
         elif Selection=='7':
+            infoSort()
+        elif Selection=='8':
             Exit=False ##exits out
     
 def IDRetrieval(): ###Finds ID and displays information
@@ -372,7 +374,50 @@ def YearRetrieval(ID):
     elif year=='2017':
         print('Senior')
 
+def infoSort():
+    studentIDList = readFile('StudentInfo.txt','r', 7)
+    file2 = readFile('StudentInfo2.txt', 'r', 8)
+    
+    colCount = int(input("How would you like to sort your data? Please select a number \n" +
+                       "0. Student ID \n" + "1. First Name \n" + "2. Last Name \n" +
+                       "3. Class Year \n" + "4. Phone Number \n" + "5. Email ID \n" + "6. Major" + '\n'))#The selection made by the user identifies the column to be switched and sorted.
+    Heading = studentIDList.pop(0)###Removes Heading
+    rowCount = 0
+    temp = ""
 
+    while(rowCount<len(studentIDList)):#This while loop swaps the identified values for each student with the first value to be sorted.
+        temp = studentIDList[rowCount][0]
+        studentIDList[rowCount][0]=studentIDList[rowCount][colCount]
+        studentIDList[rowCount][colCount]= temp
+        rowCount = rowCount+1
+    
+
+    studentIDList.sort() #sorts the first index of each list of list which was previously switched
+
+    rowCount=0 ###Needs to be 0 
+
+    while(rowCount<len(studentIDList)):#returns the now sorted list to its original format. 
+        temp = studentIDList[rowCount][0]
+        studentIDList[rowCount][0]=studentIDList[rowCount][colCount]
+        studentIDList[rowCount][colCount]= temp
+        rowCount = rowCount+1
+
+    studentIDList.insert(0,Heading) ###Puts Heading back in
+    print(" ".join(studentIDList[0]) + " " + " ".join(file2[0][1:]) + "\n")
+
+    File1counter = 1
+    while len(studentIDList)>File1counter:
+        ClassCheck = 0   ###Checking for extra semesters of courses###
+        PrintHeading=True ###Will make sure Heading prints once
+        ID = studentIDList[File1counter][0]
+        while len(file2)>ClassCheck:
+            if ID == file2[ClassCheck][0] and PrintHeading==False: ###Checks if Heading has been printed
+                print(("\t"*8) + "\t".join(file2[ClassCheck][1:])) ###Prints only relevent semesters of courses pertaining to ID
+            if ID == file2[ClassCheck][0] and PrintHeading==True:
+                print(("\t").join(studentIDList[File1counter]) + "\t" + "\t".join(file2[ClassCheck][1:])) ###Initial Heading/First Semester Listed. Use of Join
+                PrintHeading=False
+            ClassCheck +=1           
+        File1counter +=1
 
 
 
